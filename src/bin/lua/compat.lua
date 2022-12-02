984a8a6905d9b048f0da73b812a9ea0e2b609249
Slava Andrejev <vyac.andrejev@gmail.com>
2022-12-02 13:17:31 -0800

    Format all source code

--- src/bin/lua/compat.lua
+++ src/bin/lua/compat.lua
@@ -32,32 +32,33 @@
 
 rawtype = type
 
-function do_ (f, err)
-  if not f then print(err); return end
-  local a,b = pcall(f)
-  if not a then print(b); return nil
-  else return b or true
-  end
+function do_(f, err)
+    if not f then print(err); return end
+    local a, b = pcall(f)
+    if not a then print(b); return nil
+    else return b or true
+    end
 end
 
 function dostring(s) return do_(load(s)) end
+
 -- function dofile(s) return do_(loadfile(s)) end
 
 -------------------------------------------------------------------
 -- Table library
 local tab = table
-foreach = function(t,f)
-  for k,v in pairs(t) do
-    f(k,v)
-  end
+foreach = function(t, f)
+    for k, v in pairs(t) do
+        f(k, v)
+    end
 end
-foreachi = function(t,f)
-  for i,v in ipairs(t) do
-    f(i,v)
-  end
+foreachi = function(t, f)
+    for i, v in ipairs(t) do
+        f(i, v)
+    end
 end
 getn = function(t)
-  return #t
+    return #t
 end
 tinsert = tab.insert
 tremove = tab.remove
@@ -68,20 +69,20 @@ sort = tab.sort
 local dbg = debug
 getinfo = dbg.getinfo
 getlocal = dbg.getlocal
-setcallhook = function () error"`setcallhook' is deprecated" end
-setlinehook = function () error"`setlinehook' is deprecated" end
+setcallhook = function() error "`setcallhook' is deprecated" end
+setlinehook = function() error "`setlinehook' is deprecated" end
 setlocal = dbg.setlocal
 
 -------------------------------------------------------------------
 -- math library
 local math = math
 abs = math.abs
-acos = function (x) return math.deg(math.acos(x)) end
-asin = function (x) return math.deg(math.asin(x)) end
-atan = function (x) return math.deg(math.atan(x)) end
-atan2 = function (x,y) return math.deg(math.atan2(x,y)) end
+acos = function(x) return math.deg(math.acos(x)) end
+asin = function(x) return math.deg(math.asin(x)) end
+atan = function(x) return math.deg(math.atan(x)) end
+atan2 = function(x, y) return math.deg(math.atan2(x, y)) end
 ceil = math.ceil
-cos = function (x) return math.cos(math.rad(x)) end
+cos = function(x) return math.cos(math.rad(x)) end
 deg = math.deg
 exp = math.exp
 floor = math.floor
@@ -93,13 +94,13 @@ max = math.max
 min = math.min
 mod = math.mod
 PI = math.pi
---??? pow = math.pow  
+--??? pow = math.pow
 rad = math.rad
 random = math.random
 randomseed = math.randomseed
-sin = function (x) return math.sin(math.rad(x)) end
+sin = function(x) return math.sin(math.rad(x)) end
 sqrt = math.sqrt
-tan = function (x) return math.tan(math.rad(x)) end
+tan = function(x) return math.tan(math.rad(x)) end
 
 -------------------------------------------------------------------
 -- string library
@@ -131,8 +132,8 @@ tmpname = os.tmpname
 
 -------------------------------------------------------------------
 -- compatibility only
-getglobal = function (n) return _G[n] end
-setglobal = function (n,v) _G[n] = v end
+getglobal = function(n) return _G[n] end
+setglobal = function(n, v) _G[n] = v end
 
 -------------------------------------------------------------------
 
@@ -144,62 +145,61 @@ _STDERR = io.stderr
 _STDOUT = io.stdout
 _INPUT = io.stdin
 _OUTPUT = io.stdout
-seek = io.stdin.seek   -- sick ;-)
+seek = io.stdin.seek -- sick ;-)
 tmpfile = io.tmpfile
 closefile = io.close
 openfile = io.open
 
-function flush (f)
-  if f then f:flush()
-  else _OUTPUT:flush()
-  end
+function flush(f)
+    if f then f:flush()
+    else _OUTPUT:flush()
+    end
 end
 
-function readfrom (name)
-  if name == nil then
-    local f, err, cod = io.close(_INPUT)
-    _INPUT = io.stdin
-    return f, err, cod
-  else
-    local f, err, cod = io.open(name, "r")
-    _INPUT = f or _INPUT
-    return f, err, cod
-  end
+function readfrom(name)
+    if name == nil then
+        local f, err, cod = io.close(_INPUT)
+        _INPUT = io.stdin
+        return f, err, cod
+    else
+        local f, err, cod = io.open(name, "r")
+        _INPUT = f or _INPUT
+        return f, err, cod
+    end
 end
 
-function writeto (name)
-  if name == nil then
-    local f, err, cod = io.close(_OUTPUT)
-    _OUTPUT = io.stdout
-    return f, err, cod
-  else
-    local f, err, cod = io.open(name, "w")
+function writeto(name)
+    if name == nil then
+        local f, err, cod = io.close(_OUTPUT)
+        _OUTPUT = io.stdout
+        return f, err, cod
+    else
+        local f, err, cod = io.open(name, "w")
+        _OUTPUT = f or _OUTPUT
+        return f, err, cod
+    end
+end
+
+function appendto(name)
+    local f, err, cod = io.open(name, "a")
     _OUTPUT = f or _OUTPUT
     return f, err, cod
-  end
 end
 
-function appendto (name)
-  local f, err, cod = io.open(name, "a")
-  _OUTPUT = f or _OUTPUT
-  return f, err, cod
+function read(...)
+    local f = _INPUT
+    local arg = { ... }
+    if rawtype(arg[1]) == 'userdata' then
+        f = tab.remove(arg, 1)
+    end
+    return f:read(table.unpack(arg))
 end
 
-function read (...)
-  local f = _INPUT
-  local arg = {...}
-  if rawtype(arg[1]) == 'userdata' then
-    f = tab.remove(arg, 1)
-  end
-  return f:read(table.unpack(arg))
+function write(...)
+    local f = _OUTPUT
+    local arg = { ... }
+    if rawtype(arg[1]) == 'userdata' then
+        f = tab.remove(arg, 1)
+    end
+    return f:write(table.unpack(arg))
 end
-
-function write (...)
-  local f = _OUTPUT
-  local arg = {...}
-  if rawtype(arg[1]) == 'userdata' then
-    f = tab.remove(arg, 1)
-  end
-  return f:write(table.unpack(arg))
-end
-
